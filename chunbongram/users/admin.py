29a1346360b97e93d44786904e67b9d4c37839b5
chunbong <cdy0266@gmail.com>
2018-12-19 19:34:35 +0900

    Add fields to the UserAdmin fieldstes

--- chunbongram/users/admin.py
+++ chunbongram/users/admin.py
@@ -6,12 +6,16 @@ from chunbongram.users.forms import UserChangeForm, UserCreationForm
 
 User = get_user_model()
 
+# 나는 user를 업데이트 할 때 사용하는 폼이 없네..
 
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
 
     form = UserChangeForm
     add_form = UserCreationForm
-    fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
+    # followers, following에는 ManyToManyField가 입력되어 있고
+    # User 자신에게 연결되어 있다
+    # 따라서 admin panel User 탭에서 followers, following은 다수를 선택할 수 있게 보여진다
+    fieldsets = (("User", {"fields": ("name", "followers", "following")}),) + auth_admin.UserAdmin.fieldsets
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
