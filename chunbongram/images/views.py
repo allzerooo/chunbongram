6b63f63e155564e97559a1df7779fb4f7a4a3e57
chunbong <cdy0266@gmail.com>
2018-12-22 00:55:50 +0900

    Create a user feed

--- chunbongram/images/views.py
+++ chunbongram/images/views.py
@@ -6,4 +6,23 @@ class Feed(APIView):
 
     def get(self, request, format=None):
 
-        pass
+        user = request.user
+
+        following_users = user.following.all()
+
+        image_list = []
+
+        for following_user in following_users:
+            
+            # images 는 Image model 과 User 의 related_name
+            user_images = following_user.images.all()[:2]
+
+            for image in user_images:
+
+                image_list.append(image)
+        
+        sorted_list = sorted(image_list, key=lambda image: image.created_at, reverse=True)
+
+        serializer = serializers.ImageSerializer(sorted_list, many=True)
+
+        return Response(serializer.data)
