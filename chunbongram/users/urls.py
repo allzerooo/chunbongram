a5836c09c686215cfb9d513b9b01fc88020d0c49
chunbong <cdy0266@gmail.com>
2018-12-24 22:18:25 +0900

    Search users by username2

--- chunbongram/users/urls.py
+++ chunbongram/users/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path("explore/", view=views.ExploreUsers.as_view(), name="explore_users"),
     path("<int:id>/follow/", view=views.FollowUser.as_view(), name="follow_user"),
     path("<int:id>/unfollow/", view=views.UnFollowUser.as_view(), name="unfollow_user"),
+    # user_profile 이 search 보다 위에 오게 되면 user_profile 에 먼저 매칭되기 때문에 순서를 바꿈
     path("search/", view=views.Search.as_view(), name="search"),
     path("<str:username>/", view=views.UserProfile.as_view(), name="user_profile"),
     path("<str:username>/followers/", view=views.UserFollowers.as_view(), name="user_followers"),
