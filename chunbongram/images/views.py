ce9906b34158e9937a6ddde59c8c29c31f543fa9
chunbong <cdy0266@gmail.com>
2018-12-27 20:20:17 +0900

    Delete comments on my image

--- chunbongram/images/views.py
+++ chunbongram/images/views.py
@@ -178,3 +178,22 @@ class Search(APIView):
         else:
             
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class ModerateComments(APIView):
+
+    def delete(self, request, image_id, comment_id, format=None):
+
+        user = request.user
+
+        # image__id 는 Comment와 관계된 Image object의 id
+        # image__creator 는 Comment와 관계된 User object 인데
+        # image__creator__id = user__id 와 같다
+        try:
+            comment_to_delete = models.Comment.objects.get(
+                id=comment_id, image__id=image_id, image__creator=user)
+            comment_to_delete.delete()
+        except models.Comment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
