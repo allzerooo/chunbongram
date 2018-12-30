8b30a432370a5eb0a60ef80fd2a03450f44fc345
chunbong <cdy0266@gmail.com>
2018-12-30 23:30:54 +0900

    Updating the password

--- chunbongram/users/urls.py
+++ chunbongram/users/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     # user_profile 이 search 보다 위에 오게 되면 user_profile 에 먼저 매칭되기 때문에 순서를 바꿈
     path("search/", view=views.Search.as_view(), name="search"),
     path("<str:username>/", view=views.UserProfile.as_view(), name="user_profile"),
+    path("<str:username>/password/", view=views.ChangePassword.as_view(), name="change_password"),
     path("<str:username>/followers/", view=views.UserFollowers.as_view(), name="user_followers"),
     path("<str:username>/following/", view=views.UserFollowing.as_view(), name="user_following"),
 ]
--- chunbongram/users/views.py
+++ chunbongram/users/views.py
@@ -211,3 +211,48 @@ class Search(APIView):
         else:
             
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChangePassword(APIView):
+
+    def put(self, request, username, format=None):
+
+        user = request.user
+
+        # 찾을 수 없다면 'None'
+        current_password = request.data.get('current_password', None)
+
+        if user.username == username:
+
+            if current_password is not None:
+
+                # True or False
+                passwords_match = user.check_password(current_password)
+
+                if passwords_match:
+
+                    new_password = request.data.get('new_password', None)
+
+                    if new_password is not None:
+
+                        user.set_password(new_password)
+
+                        user.save()
+
+                        return Response(status=status.HTTP_200_OK)
+
+                    else:
+
+                        return Response(status=status.HTTP_400_BAD_REQUEST)
+                
+                else:
+
+                    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+            else:
+
+                return Response(status=status.HTTP_400_BAD_REQUEST) 
+
+        else:
+
+            return Response(status=status.HTTP_401_UNAUTHORIZED) 
